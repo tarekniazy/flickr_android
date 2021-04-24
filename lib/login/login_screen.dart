d06b20ca58d4b66e6c6e7bf7195541094514a2c9
ArwaShamardal <arwa.ibrahim.2000@gmail.com>
2021-04-24 14:37:49 +0200

    Finished login intial layout

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -43,41 +43,77 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(25.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          KFlickrIcon,
-          SizedBox(height: 20.0),
-          Text(
-            'Log in to Flickr',
-            style: TextStyle(
-              fontSize: KLogInToFlickrTextSize,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            KFlickrIcon,
+            SizedBox(height: 20.0),
+            Text(
+              'Log in to Flickr',
+              style: TextStyle(
+                fontSize: KLogInToFlickrTextSize,
+              ),
             ),
-          ),
-          SizedBox(height: 20.0),
-          TextField(
-            onTap: () {},
-            decoration: InputDecoration(
-              labelText: 'Email address',
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 0.8,
-                  color: Colors.blue[800],
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                //  errorText: 'Required',
+                labelText: 'Email address',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Colors.blue[800],
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4),
+                  ),
                 ),
               ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(4),
+            ),
+            SizedBox(height: 35.0),
+            Container(
+              height: 40.0,
+              width: double.infinity,
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue[600]),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  'Next',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15.0,
+                  ),
                 ),
               ),
-              // hintText: 'Email address',
-              // hintStyle: TextStyle(
-              //   color: Colors.grey,
-              // ),
             ),
-          ),
-          TextField(),
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Not a Flickr member?'),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sign up here',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
