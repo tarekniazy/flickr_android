1d7c94bad18c68449dcce57dd90702e25f307a75
tarekniazy <tarek.niazy2@gmail.com>
2021-06-02 08:48:01 +0200

    Final Integration main

--- lib/Services/networking.dart
+++ lib/Services/networking.dart
@@ -1,6 +1,11 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import '../Services/../constants.dart';
+import 'package:flickr_android/constants.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:flickr_android/constants.dart';
+import 'package:flickr_android/globals.dart' as globals;
+import 'package:dio/dio.dart';
 
 class NetworkHelper {
   NetworkHelper(this.url);
@@ -10,33 +15,26 @@ class NetworkHelper {
   Future getData(bool ifToken) async
   {
     if (ifToken)
-      {
-        var uri= Uri.parse(url);
-        Map<String, String> headers = {'Token': KUserToken};
-        print(headers);
-        http.Response response= await http.get(uri,headers:headers);
+    {
+      var uri= Uri.parse(url);
+      Map<String, String> headers = {'Token': KUserToken};
+      print(headers);
+      http.Response response= await http.get(uri,
+          headers:headers);
 
-          return response;
+      return response;
 
-        }
-        else
-        {
-          //    print(response.statusCode);
-        }
-      }
+    }
     else{
       var uri= Uri.parse(url);
       http.Response response= await http.get(uri);
-      if (response.statusCode==200)
-      {
-        return response;
 
-      }
-      else
-      {
-        //    print(response.statusCode);
-      }
+      return response;
+
     }
+
+  }
+
 
   Future putDataDio(Map<String, dynamic> Body) async {
     var uri = Uri.parse(url);
@@ -57,24 +55,25 @@ class NetworkHelper {
     print(url);
 
     if (ifToken)
-      {
-        var uri= Uri.parse(url);
-        var response = await http.post(uri,body:Body, headers: {
-          'Token': KUserToken,
-        },);
+    {
+      var uri= Uri.parse(url);
+      var response = await http.post(uri,body:Body, headers: {
+        'Token': KUserToken,
+      },);
 
-        return response;
+      return response;
 
 
-      }
+    }
     else
-      {
-        var uri= Uri.parse(url);
-        var response = await http.post(uri,body:Body,
-        );
+    {
+      var uri= Uri.parse(url);
+      var response = await http.post(uri,body:Body,
+      );
 
-        return response;
-      }
+      return response;
+
+    }
 
 
     // if (response.statusCode==200)
@@ -118,26 +117,26 @@ class NetworkHelper {
   Future deleteData() async {
     var uri = Uri.parse(url);
     var response = await http.delete(uri,
-        headers:{
-      'Token': KUserToken,
+      headers:{
+        'Token': KUserToken,
       }, //  HttpHeaders.authorizationHeader: KUserToken,
     );
     return response;
   }
 
-  // Future deleteData() async {
-  //   var uri = Uri.parse(url);
-  //   var dio = Dio();
-  //
-  //   Response response = await dio.delete(uri.toString(),
-  //       data: jsonEncode(Body),
-  //       options: Options(headers: {
-  //         'Token': KUserToken,
-  //       }));
-  //   print(response.data);
-  //   print(response.statusMessage);
-  //   return response;
-  // }
+// Future deleteData() async {
+//   var uri = Uri.parse(url);
+//   var dio = Dio();
+//
+//   Response response = await dio.delete(uri.toString(),
+//       data: jsonEncode(Body),
+//       options: Options(headers: {
+//         'Token': KUserToken,
+//       }));
+//   print(response.data);
+//   print(response.statusMessage);
+//   return response;
+// }
 
 
 }
