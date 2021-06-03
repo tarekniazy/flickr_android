5b2852937b3b25903c5daef925b947c91d9b33af
ArwaShamardal <arwa.ibrahim.2000@gmail.com>
2021-06-04 00:43:42 +0200

    Added texts/textfields widget_test to signin

---
+++ test/widgetTest/screensWidgetTesting/signupWidgetTest.dart
@@ -0,0 +1,22 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import '../main_widgets_test.dart';
+import 'package:flickr_android/signup/signup_screen.dart';
+
+void main() {
+  Widget testWidget = new MediaQuery(
+      data: new MediaQueryData(), child: new MaterialApp(home: SignUpScreen()));
+
+  TesterOfWidgets testerOfWidgets = TesterOfWidgets(testWidget);
+  testerOfWidgets.findTextOnce(textToFind: 'Sign Up for Flickr');
+  testerOfWidgets.findTextOnce(textToFind: 'flickr');
+  testerOfWidgets.findTextOnce(textToFind: 'Already a Flickr member?');
+  testerOfWidgets.findTextField(
+      textFieldToEnter: 'Entered Text', numberOfTextField: 5);
+}
